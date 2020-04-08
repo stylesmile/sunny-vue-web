ed07068381fc3fb0273a7a1514fbf739a56f5d74
chenye <15002770045@139.com>
2020-04-08 13:29:16 +0800

    权限拦截

--- src/router/index.js
+++ src/router/index.js
@@ -7,17 +7,33 @@ import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(Router)
 Vue.use(elementui)
 
-export default new Router({
-  routes: [
-    {
-      path: '/hello',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/',
-      name: 'login',
-      component: () => import('@/views/login.vue')
-    }
-  ]
+export const constantRoutes = [
+  {
+    path: '/hello',
+    name: 'HelloWorld',
+    component: HelloWorld
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login.vue')
+  },
+  {
+    path: '/',
+    name: 'login',
+    component: () => import('@/views/login.vue')
+  }]
+
+const createRouter = () => new Router({
+  // mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRoutes
 })
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter () {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
+}
+const router = createRouter()
+
+export default router
